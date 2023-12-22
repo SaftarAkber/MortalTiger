d969e3e1c8b14e7161f6156cd81b43a6d9afce2b
SaftarAkbar <saftarea@code.edu.az>
2023-12-23 03:41:35 +0400

    Buttons, newly Header design

--- src/constants/images.js
+++ src/constants/images.js
@@ -1,6 +1,6 @@
 import Logo from '../assets/Digitalism/Logo.png';
-import bg from '../assets/Digitalism/bg.jpg'
-import NFT from '../assets/Digitalism/NFT.png'
+import bg from '../assets/Digitalism/bg1.png'
+import NFT from '../assets/Digitalism/NFT.png';
 export default {
   Logo,
   bg,
--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -5,21 +5,19 @@ import "./Header.css";
 const Header = () => (
   <div className="header__main">
     <div className="header__text">
-      <h1 className="header__text-coname">MORTAL TIGER</h1>
+      <h1 className="header__text-coname image">MORTAL TIGER</h1>
       <p className="header__text-info">
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Neque illum
-        doloremque suscipit asperiores dignissimos vero libero sint laborum odit
-        expedita magni, tempore nisi molestias voluptate voluptatibus ex
-        incidunt iure itaque. Reprehenderit provident mollitia accusamus,
-        doloribus cum repellat iusto. A aspernatur sint quo nihil sed nulla
-        consequuntur totam vel odit repellendus, veniam ipsa quas dolorum?
-        Labore dolores tempore hic sapiente minima?
+        Welcome to Mortal Tiger, where gaming meets a cause! Immerse yourself in
+        Muay Thai Unleashed, our thrilling mobile game, and become a guardian of
+        big cats worldwide. Roar with us—Mortal Tiger isn't just a game; it's a
+        movement for a wilder, safer future. Do charity and get a chance to earn
+        more by becoming an esportsman. Change the World with us!
       </p>
     </div>
-    <button className="header__button-play">PLAY</button>
-    <button className="header__button-donate">DONATE</button>
+    <button className="header__button-play btn">PLAY</button>
+    <button className="header__button-donate  btn">DONATE</button>
     <div className="header__nft">
-      <img src={images.NFT}></img>
+      <img src={images.NFT} alt="Tiger NFT"></img>
     </div>
   </div>
 );
