192fa7a7a55c917d232747034f790e7df0e95cb2
SaftarAkbar <saftarea@code.edu.az>
2023-12-25 04:17:05 +0400

    About us part 1

--- src/container/AboutUs/AboutUs.jsx
+++ src/container/AboutUs/AboutUs.jsx
@@ -3,7 +3,10 @@ import React from 'react';
 import './AboutUs.css';
 
 const AboutUs = () => (
-  <div>
+  <div className='aboutus__header'>
+    <div className='box'>
+      <p>What is Mortal Tiger</p>
+    </div>
   </div>
 );
 
