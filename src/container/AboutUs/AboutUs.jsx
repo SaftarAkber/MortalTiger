ccc357edfc0d408a3fa93c06df24c33d81a50cf4
Saftar Akbarov <74908380+SaftarAkber@users.noreply.github.com>
2023-12-26 23:08:03 +0000

    Grid system

--- src/container/AboutUs/AboutUs.jsx
+++ src/container/AboutUs/AboutUs.jsx
@@ -4,8 +4,11 @@ import './AboutUs.css';
 
 const AboutUs = () => (
   <div className='aboutus__header'>
-    <div className='box'>
-      <p className='text-7xl	text-gray-800'>What is Mortal Tiger</p>
+    <div class="grid grid-rows-3 grid-flow-col gap-3">
+      <div class="bg-black  row-span-3  h-screen w-full ...">01</div>
+      <div class="bg-green-800 col-span-1  w-full row-span-1 ...">02</div>
+      <div class="bg-emerald-800 row-span-1 w-full col-span-1 ...">03</div>
+      <div class="bg-blue-800 row-span-1 w-full col-span-1 ...">04</div>
     </div>
   </div>
 );
